34a57afed91f48db5974f1899cc06e7b2c28e013
MrJujek <mr.jujek@gmail.com>
2024-11-13 21:06:55 +0100

    ✨ Check system info

--- scripts/install.ps1
+++ scripts/install.ps1
@@ -32,6 +32,17 @@ function success {
     Write-Host $message -ForegroundColor $color
 }
 
+if (-not ((Get-CimInstance Win32_ComputerSystem)).SystemType -match "x64-based") {
+    error "Tiny-git-helper is currently not available on 32-bit or ARM system."
+}
+
+$MinBuild = 17763;
+$MinBuildName = "Windows 10 1809"
+$WinVer = [System.Environment]::OSVersion.Version
+if ($WinVer.Major -lt 10 -or ($WinVer.Major -eq 10 -and $WinVer.Build -lt $MinBuild)) {
+  error "Tiny-git-helper requires version ${MinBuildName} or newer."
+}
+
 info "This is an info message"
 info_bold "This is an info message in bold"
 success "This is a success message"
